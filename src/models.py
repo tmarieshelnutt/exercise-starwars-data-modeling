c2345d0b0f6fe8590cbb0388c2e03b8a0e798ecd
Tara <tmarieshelnutt@gmail.com>
2023-06-03 02:18:44 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -26,7 +26,49 @@ class Address(Base):
     person = relationship(Person)
 
     def to_dict(self):
-        return {}
+        return {
+            "<User(name, lastname, username)>"
+            "name": self.name
+        }
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+
+class character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), primary_key=False)
+    height = Column(String(250), nullable=False)
+    mass = Column(String(250), nullable=False)
+    hair_color = Column(String(250), primary_key=False)
+    eye_color = Column(String(250), primary_key=False)
+    skin_color = Column(String(250), primary_key=False)
+    birth_year = Column(String(250), nullable=False)
+    gender = Column(String(250), primary_key=False)
+    hoeworld = Column(Strin(250), primary_key=False)
+    
+class fav_character(Base):
+    __tablename__ = "fav_character"
+    id = Column(Integer, ForeignKey("user.id"))
+    user_id = Column(Integer, ForeignKey("user.name"))
+    character_id = Column(Integer, ForeignKey("character.id"))
+    
+class planets(Base):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), primary_key=False)
+    rotation_period = Column(Integer, nullable=False)
+    orbital_period = Column(Integer, nullable=False)
+    diameter = Column(Integer, primary_key=False)
+    climate = Column(String(250), primary_key=False)
+    gravity = Column(Integer, nullable=False)
+    terrain = Column(String(250), nullable=False)
+    surface_water = Column(Integer, nullable=False)
+    population = Column(Integer, nullable=False)
+
+
+class fav_planets(Base):
+    __tablename__ = "fav_planets"
+    id = Column(Integer, ForeignKey("user.id"))
+    user_id = Column(Integer, ForeignKey("user.name"))
+    planets_id = Column(Integer, ForeignKey("planet.id"))
+    
